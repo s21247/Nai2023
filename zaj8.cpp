9e142abc55533083a92e818752c5f9b23954f0a6
Wojciech Piórecki <56393403+s21247@users.noreply.github.com>
2023-01-10 21:25:39 +0100

    Update zaj8.cpp

--- zaj8.cpp
+++ zaj8.cpp
@@ -8,58 +8,75 @@ using namespace cv;
 
 int main(int argc, char** argv) {
 
-    int down_range[3] = { 0,90,150 }, up_range[3] = { 90,255,255 };
+    int lower_range[3] = { 0,100,120 }, higher_range[3] = { 45,190,185 };
 
-    VideoCapture camera(0);
+    VideoCapture cap(0);
 
-    while (waitKey(1) != 27 || waitKey(1) == 'q') {
+    do {
 
         Mat frame;
-        Mat frameMaskOne, frameMaskTwo;
+        Mat frameMask, frameMask2;
         Mat frameWithMask;
 
-        camera >> frame;
+        cap >> frame;
 
         flip(frame, frame, 1);
 
-        cvtColor(frame, frameMaskOne, COLOR_BGR2HSV);
-        inRange(frameMaskOne, Scalar(down_range[0], down_range[1], down_range[2]),
-            Scalar(up_range[0], up_range[1], up_range[2]), frameMaskTwo);
+        cvtColor(frame, frameMask, COLOR_BGR2HSV);
+        inRange(frameMask, Scalar(lower_range[0], lower_range[1], lower_range[2]),
+            Scalar(higher_range[0], higher_range[1], higher_range[2]), frameMask2);
 
         vector<vector<Point>>  contours;
-        findContours(frameMaskTwo, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
+        findContours(frameMask2, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
 
-        //sortowanie konturów po wielkości
         sort(contours.begin(), contours.end(),
             [](auto& a, auto& b) {
                 return contourArea(a, false) > contourArea(b, false);
             });
 
 
-        Point center[2] = {
+        Point middle[2] = {};
+        Point middle2[2] = {};
+        Point middle3[2]= {};
 
-        };
 
-        int changableLine = 10;
-        if (contours.size() >= 2 && contourArea(contours[0]) != 0 && contourArea(contours[1]) != 0) {
+
         for (int i = 0; i < min(2, (int)contours.size()); i++)
         {
             approxPolyDP(contours.at(i), contours.at(i), 10, true);
             drawContours(frame, contours, i, { 0,0,255,255 });
             Rect rect = boundingRect(contours.at(i));
-            //cout << (int(abs(contourArea(contours[0])/100))) << endl;
-            center[i].x = rect.width / 2 + rect.x;
-            center[i].y = rect.height / 2 + rect.y;
+            middle[i].x = rect.width / 2 + rect.x;
+            middle[i].y = rect.height / 2 + rect.y;
         }
-        
-        if (abs((center[0].y - center[1].y <= 30))) {
-            line(frame, center[0], center[1], Scalar(200, 0, 0), (int(abs(contourArea(contours[0]) / 100))));
+
+        for (int i = 0; i < min(2, (int)contours.size()); i++)
+        {
+            approxPolyDP(contours.at(i), contours.at(i), 10, true);
+            drawContours(frame, contours, i, { 0,0,255,255 });
+            Rect rect = boundingRect(contours.at(i));
+            middle2[i].y = rect.height + 2 / 2 + rect.y;
+            middle2[i].x = rect.width / 2 + rect.x;
+            middle3[i].y = rect.height - 2 / 2 + rect.y;
+            middle3[i].x = rect.width / 2 + rect.x;
         }
-    }
+
+        if (abs((middle[0].y - middle[1].y < 30))) {
+
+            if (contours.size() % 2 != 0) {
+                line(frame, middle[0], middle[1], Scalar(200, 0, 0), 10);
+                line(frame, middle2[0], middle2[1], Scalar(200, 0, 0), 8);
+                line(frame, middle3[0], middle3[1], Scalar(200, 0, 0), 8);
+            }
+            else {
+                line(frame, middle[0], middle[1], Scalar(200, 0, 0), 10);
+                line(frame, middle2[0], middle2[1], Scalar(200, 0, 0), 8);
+
+            }
+            
+        }
         imshow("contours", frame);
 
-        if (waitKey(1) == 'q')
-            break;
-    }
+    } while (waitKey(1) != 27);
     return 0;
 }
